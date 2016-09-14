106bba82f8d0617bbf66a042be8930714ec8aba8
nhanb <nhan@nerdyweekly.com>
2016-09-14 17:27:21 +0700

    sorry Diego :(

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function() {
 
   $('#trailer-modal').on('shown.bs.modal', function () {
-    $("#player").attr('src', 'https://www.youtube.com/embed/jQ_2iIqxH7Y?rel=0&autoplay=1&showinfo=0&wmode=transparent');
+    $("#player").attr('src', 'https://www.youtube.com/embed/LqN2tX3LTJA?rel=0&autoplay=1&showinfo=0&wmode=transparent');
   });
 
   $("#trailer-modal").on('hidden.bs.modal', function (e) {
